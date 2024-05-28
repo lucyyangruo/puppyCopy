df6db807e1329ab36cb5eac59dfb7cb3f4ad6cf0
Laween Alexis <jeanettewilliameq@gmail.com>
2024-05-28 13:47:15 +0800

    添加了dog的依赖

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lucyyangruo/puppyCopy
 
 go 1.22.3
+
+require github.com/lucyyangruo/dogCopy v0.0.0-20240528054013-a8e62aa2b62a // indirect
--- puppyCopy.go
+++ puppyCopy.go
@@ -1,9 +1,20 @@
 package puppycopy
 
+import (
+	dogcopy "github.com/lucyyangruo/dogCopy"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark(s string) string {
+	return dogcopy.WhenGrowUp(Bark())
+}
+func BigBarks(s string) string {
+	return dogcopy.WhenGrowUp(Barks())
 }
